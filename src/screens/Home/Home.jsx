ae42ca9d21cff668e84959263895901cdafb2ecc
Alexandre Pereira <xandedrop2@gmail.com>
2024-07-29 00:05:15 -0300

    .

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -9,19 +9,19 @@ const Home = () => {
             jsonPath="ticketmedio.json" // Caminho para o arquivo JSON
             title="Ticket Médio" 
             keyToDisplay="TicketMedio" // Chave para exibir do JSON
-            link="/Ticket"
+            link="/front-end/ticket"
         />
         <OutlinedCard 
             jsonPath="redirecionamento.json" // Caminho para o arquivo JSON
             title="Maiores clientes por cidade" 
             keyToDisplay="button" // Chave para exibir do JSON
-            link="/clientes"
+            link="/front-end/clientes"
         />
         <OutlinedCard 
             jsonPath="resultado_vendas.json" // Caminho para o arquivo JSON
             title="Modelo mais vendido" 
             keyToDisplay="modelo_mais_vendido" // Chave para exibir do JSON
-            link="/modelos"
+            link="/front-end/modelos"
         />
     </div>
   )
